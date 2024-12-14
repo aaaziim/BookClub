f872cc1adf2a14780cb86a80ca75927ecc5e320f
aaaziim <azim210215@gmail.com>
2024-12-15 02:30:23 +0600

    Home Banner Added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,39 @@
-
+import { NavLink } from "react-router-dom";
+import bannerImg from "../assets/bannerImg.png"
+import { TypeAnimation } from 'react-type-animation';
 const Banner = () => {
     return (
         <div>
-            Banner
+            <section className="my-6 dark:bg-gray-100 dark:text-gray-800 rounded-2xl">
+                <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                        <h1 className="text-3xl font-bold   ">
+                            <TypeAnimation
+                                sequence={[
+                                    'Explore with us',
+                                    1500,
+                                    'Make it happen',
+                                    2000,
+                                ]}
+                                wrapper="span"
+                                speed={50}
+                                style={{ fontSize: '2em', display: 'inline-block' }}
+                                repeat={Infinity}
+                            /></h1>
+
+                        <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start my-6">
+                            <NavLink to="/listedbooks">
+                                <button type="button" className="my-3 text-white text-2xl bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg   px-5 py-2.5 text-center me-2 mb-2">View The List</button>
+                            </NavLink>
+
+
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                        <img src={bannerImg} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
 const Header = () => {
     return (
@@ -22,13 +23,13 @@ const Header = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Home</a></li>
-
-                        <li><a>Listed Books</a></li>
-                        <li><a>Pages to Read</a></li>
+                        {<NavLinks></NavLinks>}
                     </ul>
                 </div>
-                <a className="text-3xl text-black font-bold hover:text-[#23BE0A] hover:border-2 hover:border-bg-[#23BE0A]  p-2 hover:rounded-xl">BookClub</a>
+                <NavLink to="/" className="text-3xl text-black font-bold hover:text-[#23BE0A] hover:border-2 hover:border-bg-[#23BE0A]  p-2 hover:rounded-xl">
+                    BookClub
+                </NavLink>
+
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu  text-xl menu-horizontal px-1 text-black font-medium">
@@ -36,8 +37,8 @@ const Header = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign In</button>
-                <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign Up</button>
+                <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign In</button>
+                <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign Up</button>
 
             </div>
         </div>
